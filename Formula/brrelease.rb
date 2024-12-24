db85dc048cf81585c63c8a375dd3947d04df58fe
Kerren Ortlepp <kerren@entrostat.com>
2024-12-24 10:27:25 +0200

    chore: Update to v1.14.0

--- Formula/brrelease.rb
+++ Formula/brrelease.rb
@@ -1,26 +1,26 @@
 class Brrelease < Formula
   desc "Install script for the brrelease CLI"
   homepage "https://github.com/kerren/brrelease"
-  url "https://github.com/kerren/brrelease/releases/download/v1.13.0/brrelease-v1.13.0-282cc37-darwin-x64.tar.xz"
-  version "1.13.0"
-  sha256 "d4231650ab51c0a7329088de3ef3fd03ba013260efa7897666ece095ccd63ca5"
+  url "https://github.com/kerren/brrelease/releases/download/v1.14.0/brrelease-v1.14.0-677383a-darwin-x64.tar.xz"
+  version "1.14.0"
+  sha256 "231cf672eca7e82d47c464f20772cf6aaf5a24806213ddb19eba5493b04d56dc"
   version_scheme 1
 
   on_macos do
     on_arm do
-      url "https://github.com/kerren/brrelease/releases/download/v1.13.0/brrelease-v1.13.0-282cc37-darwin-arm64.tar.xz"
-      sha256 "213f4de6352333f60bef5e208f4a9fb7b7a7231f9b5ebfdcb80efe49e0e78ded"
+      url "https://github.com/kerren/brrelease/releases/download/v1.14.0/brrelease-v1.14.0-677383a-darwin-arm64.tar.xz"
+      sha256 "2d30732dc3af4e58ab690f5d0d64244bd9ce08a0f15f0f5e393cfaaf5508ab41"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/kerren/brrelease/releases/download/v1.13.0/brrelease-v1.13.0-282cc37-linux-x64.tar.xz"
-      sha256 "28fdb6e8be09ddb1ada739514c6eb5683134950c7cf360c8a3cb1be9e3184f5b"
+      url "https://github.com/kerren/brrelease/releases/download/v1.14.0/brrelease-v1.14.0-677383a-linux-x64.tar.xz"
+      sha256 "7f6f4770cf8e5129bde5e707072cfa208082c47ccfc110319cd40bb4a387ed6c"
     end
     on_arm do
-      url "https://github.com/kerren/brrelease/releases/download/v1.13.0/brrelease-v1.13.0-282cc37-linux-arm64.tar.xz"
-      sha256 "b3ef79151e43680235137dbf62a0c4e543c2ab5797108e793fced1ed54c8fd76"
+      url "https://github.com/kerren/brrelease/releases/download/v1.14.0/brrelease-v1.14.0-677383a-linux-arm64.tar.xz"
+      sha256 "b1aca85b5be3bc1e2cf978cfbf2269868a4ae418309f2767b9d5351b80179b35"
     end
   end
 
