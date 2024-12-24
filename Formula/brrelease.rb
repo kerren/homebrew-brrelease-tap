be3b415c0d3911719bfbc91000e1b143eb08918e
Kerren Ortlepp <kerren@entrostat.com>
2024-12-24 09:50:55 +0200

    chore: Update to v1.13.0

--- Formula/brrelease.rb
+++ Formula/brrelease.rb
@@ -1,26 +1,26 @@
 class Brrelease < Formula
   desc "Install script for the brrelease CLI"
   homepage "https://github.com/kerren/brrelease"
-  url "https://github.com/kerren/brrelease/releases/download/v1.12.0/brrelease-v1.12.0-b559dc0-darwin-x64.tar.xz"
-  version "1.12.0"
-  sha256 "613a4be894b49e5275051b1e05d08e1263754257992a46957627a18ad5528b68"
+  url "https://github.com/kerren/brrelease/releases/download/v1.13.0/brrelease-v1.13.0-282cc37-darwin-x64.tar.xz"
+  version "1.13.0"
+  sha256 "d4231650ab51c0a7329088de3ef3fd03ba013260efa7897666ece095ccd63ca5"
   version_scheme 1
 
   on_macos do
     on_arm do
-      url "https://github.com/kerren/brrelease/releases/download/v1.12.0/brrelease-v1.12.0-b559dc0-darwin-arm64.tar.xz"
-      sha256 "c08e21bfa09c13d960e343a0e03d1b0f33088fa2c75cf937b555a9c86c9c1bfc"
+      url "https://github.com/kerren/brrelease/releases/download/v1.13.0/brrelease-v1.13.0-282cc37-darwin-arm64.tar.xz"
+      sha256 "213f4de6352333f60bef5e208f4a9fb7b7a7231f9b5ebfdcb80efe49e0e78ded"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/kerren/brrelease/releases/download/v1.12.0/brrelease-v1.12.0-b559dc0-linux-x64.tar.xz"
-      sha256 "b9308ab66f9f8a4f930d5cfb055ae9c9df681a2bc8741ecf70a0fca93e5a073c"
+      url "https://github.com/kerren/brrelease/releases/download/v1.13.0/brrelease-v1.13.0-282cc37-linux-x64.tar.xz"
+      sha256 "28fdb6e8be09ddb1ada739514c6eb5683134950c7cf360c8a3cb1be9e3184f5b"
     end
     on_arm do
-      url "https://github.com/kerren/brrelease/releases/download/v1.12.0/brrelease-v1.12.0-b559dc0-linux-arm64.tar.xz"
-      sha256 "3494c7473c63efd3e64eefc136ff4906a137352b2a8ea03bfa3fa8eec49ac287"
+      url "https://github.com/kerren/brrelease/releases/download/v1.13.0/brrelease-v1.13.0-282cc37-linux-arm64.tar.xz"
+      sha256 "b3ef79151e43680235137dbf62a0c4e543c2ab5797108e793fced1ed54c8fd76"
     end
   end
 
